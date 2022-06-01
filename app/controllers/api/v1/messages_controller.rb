399231fc0888ea422bbd1d01629a37d8be63edaf
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-05-31 20:18:41 -0400

    Apply rubocop format

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::MessagesController < ApplicationController
   def index
-		message = Message.find(Message.pluck(:id).sample)
-		render json: {
-			message: message.text
-		}.to_json
-	end
+    message = Message.find(Message.pluck(:id).sample)
+    render json: {
+      message: message.text
+    }.to_json
+  end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,3 @@
 class MessagesController < ApplicationController
-
-  def index 
-	
-	end
-
-  
+  def index; end
 end
